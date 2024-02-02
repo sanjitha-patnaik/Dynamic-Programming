567f4ff1174a44845c0f8096d8a044b3dd044cfe
Sanjitha Jaggumantri <108885162+sanjitha-patnaik@users.noreply.github.com>
2024-02-03 00:08:21 +0530

    Update Longest Increasing Subsequence.py

--- Longest Increasing Subsequence.py
+++ Longest Increasing Subsequence.py
@@ -1,1 +1,5 @@
+# Longest Increasing Subsequence
+# Problem: Find the length of the longest subsequence common to two sequences.
+# Approach: Use dynamic programming to store the length of the longest common subsequence for substrings.
+
 
